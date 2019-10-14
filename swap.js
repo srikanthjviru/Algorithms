39c1ad3b32a83e43d29fb8c4874b46a10623fea7
Srikanth <srikanthjviru@gmail.com>
2019-10-13 23:09:19 -0500

    added comment in swap

--- swap.js
+++ swap.js
@@ -1,10 +1,12 @@
-function swap(array, i, j){
-  console.log(`Given array ${array} and swaping ${i} with ${j}`)
+function swap(array, i, j) {
+  console.log(`Given array ${array} and swaping ${i} with ${j}`);
   let temp = array[i];
-  array[i]= array[j];
-  array[j] = temp
-  console.log(`swap is ${array} `)
-  return array
+  array[i] = array[j];
+  array[j] = temp;
+  console.log(`swap is ${array} `);
+  return array;
 }
 
-swap([1,2,3,4,5,6,7,8], 2,4);
+swap([1, 2, 3, 4, 5, 6, 7, 8], 2, 4);
+
+// Swap here!!
