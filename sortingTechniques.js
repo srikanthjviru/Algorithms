e7c9164b6ddd8e92e0c5f53b89af2c6c694698a5
srikanthjviru <srikanthjviru@gmail.com>
2022-11-23 11:42:42 -0500

    Update sortingTechniques.js
    Updated sorting technique..

--- sortingTechniques.js
+++ sortingTechniques.js
@@ -20,3 +20,26 @@ const selectionSort = arr => {
 
 selectionSort([43, 3, 454, 5, 343, 6]);
 selectionSort(["cat", "bat", "eat", "fight", "cool", "crawll"]);
+
+//  ------- ----- ------ Insertion sort ----- ----- ---------- // 
+
+function insertionSort(inputArr) {
+    let n = inputArr.length;
+        for (let i = 1; i < n; i++) {
+            // Choosing the first element in our unsorted subarray
+            let current = inputArr[i];
+            // The last element of our sorted subarray
+            let j = i-1; 
+            while ((j > -1) && (current < inputArr[j])) {
+                inputArr[j+1] = inputArr[j];
+                j--;
+            }
+            inputArr[j+1] = current;
+        }
+    return inputArr;
+};
+let inputArr = [5, 2, 4, 6, 1, 3];
+insertionSort(['sihd','ereetert','yyytyrt','apple','caee','grrraet!!!'])
+insertionSort(inputArr);
+console.log(inputArr);
+
